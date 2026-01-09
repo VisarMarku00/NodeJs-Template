c8f2c2e4f775ae38095e2cc0177a9ecf7461d54f
VisarMarku00 <96630229+VisarMarku00@users.noreply.github.com>
2026-01-09 12:29:11 +0100

    removed babel

--- webpack.common.js
+++ webpack.common.js
@@ -14,17 +14,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-            
-          },
-        },
-      },
-      {
         test: /\.html$/i,
         loader: "html-loader",
       },
